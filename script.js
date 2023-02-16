167e198b22bb8d8ccaf0ddb6677118ba81f6b1b8
Łukasz Śmigiel <81709506+L-Smigiel@users.noreply.github.com>
2023-02-16 19:57:21 +0100

    finished

--- script.js
+++ script.js
@@ -1,35 +1,57 @@
 let counter = 1;
-      console.log(counter);
-      const interval = 6000;
-      let sliderInterval = () => {
-        setInterval(() => {
-          counter++;
-          if (counter > 8) {
-            counter = 1;
-          }
-          console.log(counter);
-          document.getElementById("radio" + counter).checked = true;
-        }, interval);
-      };
-      sliderInterval();
-      let moveImgLeft = () => {
-        clearInterval(sliderInterval);
-        if (counter <= 1) {
-          counter = 8;
-        } else {
-          counter--;
-        }
-        console.log(counter);
-        document.getElementById("radio" + counter).checked = true;
-      };
-      let moveImgRight = () => {
-        clearInterval(sliderInterval);
-        if (counter >= 8) {
-          counter = 1;
-        } else {
-          counter++;
-        }
-        console.log(counter);
-        document.getElementById("radio" + counter).checked = true;
-      };
-      console.log(interval);
+let myInterval = null;
+let checkFirstRadioByDefault = () => {
+  document.getElementById("radio1").checked = true;
+};
+
+checkFirstRadioByDefault();
+console.log(counter);
+const interval = 6000;
+let sliderInterval = () => {
+  clearInterval(myInterval)
+  myInterval = setInterval(() => {
+    counter++;
+    if (counter > 8) {
+      counter = 1;
+    }
+    console.log(counter);
+    document.getElementById("radio" + counter).checked = true;
+  }, interval);
+};
+sliderInterval();
+let moveImgLeft = () => {
+  clearInterval(myInterval);
+  if (counter <= 1) {
+    counter = 8;
+  } else {
+    counter--;
+  }
+  console.log(counter);
+  document.getElementById("radio" + counter).checked = true;
+  sliderInterval();
+};
+let moveImgRight = () => {
+  clearInterval(myInterval);
+  if (counter >= 8) {
+    counter = 1;
+  } else {
+    counter++;
+  }
+  console.log(counter);
+  document.getElementById("radio" + counter).checked = true;
+  sliderInterval();
+};
+console.log(interval);
+
+//jQuery below
+$(".radio").click(function () {
+  const index = $(".radio").index(this);
+  myFunction(index);
+});
+
+let myFunction = (index) => {
+  counter = index + 1;
+  clearInterval(myInterval);
+  document.getElementById("radio" + counter).checked = true;
+  sliderInterval();
+};
